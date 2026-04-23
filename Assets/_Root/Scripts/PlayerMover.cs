723410b856bbf31c2ea7f8cdb7f6e3b8f2a266d7
YuriiSushko <ura12334567@gmail.com>
2026-04-23 22:19:49 +0300

    [feat] it players it moves

---
+++ Assets/_Root/Scripts/PlayerMover.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 4f;
+
+    [SerializeField]
+    private float minX = -2.35f;
+
+    [SerializeField]
+    private float maxX = 2.35f;
+
+    void Update()
+    {
+        float input = Input.GetAxisRaw("Horizontal");
+
+        Vector3 pos = transform.position;
+        pos.x += input * moveSpeed * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+
+        transform.position = pos;
+    }
+}
