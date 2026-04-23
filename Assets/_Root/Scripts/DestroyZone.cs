30406984dea46920df55e09190633f6694d510d3
YuriiSushko <ura12334567@gmail.com>
2026-04-23 22:47:49 +0300

    [feat] objects spawns and destroys

---
+++ Assets/_Root/Scripts/DestroyZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DestroyZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<FallingObject>() != null)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/_Root/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fallingObjectPrefab;
+
+    [SerializeField]
+    private float spawnInterval = 1f;
+
+    [SerializeField]
+    private float minX = -2.2f;
+
+    [SerializeField]
+    private float maxX = 2.2f;
+
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnObject();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnObject()
+    {
+        float randomX = Random.Range(minX, maxX);
+        Vector3 spawnPosition = new Vector3(randomX, transform.position.y, 0f);
+
+        Instantiate(fallingObjectPrefab, spawnPosition, Quaternion.identity);
+    }
+}
